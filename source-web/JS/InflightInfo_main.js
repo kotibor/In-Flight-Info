ef8cd5c9bf832b9742636c2d4c2acead4c2ac102
kotibor <32471207+kotibor@users.noreply.github.com>
2018-01-14 12:38:26 +0800

    Add files via upload

---
+++ source-web/JS/InflightInfo_main.js
@@ -0,0 +1,6 @@
+var airline = prompt("What airline do you travel with?");
+var flightNumber = prompt("What is the flight number?");
+document.write(airline + " " + flightNumber);
+
+var origin
+var destination
